8e887c13046c83efa910035bb27185a8a783b7bf
Olivier DALET <odalet@gmail.com>
2016-06-26 17:13:13 +0200

    tests

--- TestJenkinsSonarMSTest/CppLib/CppCalc.cpp
+++ TestJenkinsSonarMSTest/CppLib/CppCalc.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppCalc.h"
 
+void foo(int unused) {}
 
 double CppCalc::add(double a, double b) { return a + b; }
 double CppCalc::sub(double a, double b) { return a - b; }
