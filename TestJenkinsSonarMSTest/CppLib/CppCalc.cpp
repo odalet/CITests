c7218947c3014b891c87d76c7cefdd04bf7c1c36
Olivier DALET <odalet@gmail.com>
2016-06-26 15:31:05 +0200

    Added C++ lib

---
+++ TestJenkinsSonarMSTest/CppLib/CppCalc.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "CppCalc.h"
+
+
+double CppCalc::add(double a, double b) { return a + b; }
+double CppCalc::sub(double a, double b) { return a - b; }
+double CppCalc::mul(double a, double b) { return a * b; }
+double CppCalc::div(double a, double b) { return a / b; }
+
+CppCalc::CppCalc(){ }
+CppCalc::~CppCalc() { }
---
+++ TestJenkinsSonarMSTest/CppLib/CppCalc.h
@@ -0,0 +1,15 @@
+#pragma once
+class CppCalc
+{
+public:
+
+	static double add(double a, double b);
+	static double sub(double a, double b);
+	static double mul(double a, double b);
+	static double div(double a, double b);
+
+private:
+	CppCalc();
+	~CppCalc();
+};
+
